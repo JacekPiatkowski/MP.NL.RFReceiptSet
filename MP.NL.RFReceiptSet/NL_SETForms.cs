1fce11439988123375b8dd77525b2900dc63f2c0
Jacek Piątkowski <piatkowskij@mantis.email>
2018-07-30 12:21:25 +0200

    test ??

--- MP.NL.RFReceiptSet/NL_SETForms.cs
+++ MP.NL.RFReceiptSet/NL_SETForms.cs
@@ -15,20 +15,21 @@ namespace MP.NL.RFReceiptSet
 	class NL_SETForms : Mantis.LVision.RFApi.RFForm
 	{
 		private System.Data.DataSet m_dsTask = null;
-		private Mantis.LVision.RFApi.RFForm m_frm;
+		private Mantis.LVision.RFApi.RFForm m_Form;
 
 		public override bool Start()
 
 		{
 
-			/*	string su, ipbstrResult;
-	ipbstrResult = null;
-	System.Data.DataSet ds;
-	strSQL = "select '1' id, 'TAK' rep UNION ALL  select '2' id, 'NIE' rep";
+			string su, ipbstrResult, strSQL;
+			ipbstrResult = null;
+			System.Data.DataSet ds;
+			strSQL = "select '1' id, 'TAK' rep UNION ALL  select '2' id, 'NIE' rep";
+			Mantis.LVision.DBAccess.DbRoutines.Routines dbr = new Mantis.LVision.DBAccess.DbRoutines.Routines();
 
-	ds = dbr.SelectTable(strSQL, m_Form, null);
-	su = m_Form.Rf.DisplayCombo(1, 1, "Produkt: "+ProductCode+(char)13+"Typ SET: " + IA_SetValue.ToString(), "Zmiana? : ", ref ipbstrResult, ds.Tables[0], "id", "id", "rep", eEchoMode.ECHO_ON, null, true, false, "1", true);
-	*/
+			ds = dbr.SelectTable(strSQL, m_Form, null);
+			su = m_Form.Rf.DisplayCombo(1, 1, "Produkt: "+ProductCode+(char)13+"Typ SET: " + IA_SetValue.ToString(), "Zmiana? : ", ref ipbstrResult, ds.Tables[0], "id", "id", "rep", eEchoMode.ECHO_ON, null, true, false, "1", true);
+	
 
 
 			return false;
